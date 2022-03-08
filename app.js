3ea3c5afacde22ddb915507503e07d6920245605
Logan Morro <loganmlogan@gmail.com>
2022-03-08 12:12:44 -0500

    starting template created

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const bodyParser = requrie("body-parser");
+const mongoose = require("mongoose");
+const ejs = require("ejs");
+
+const app = express();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
